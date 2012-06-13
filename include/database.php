d2911ccc43c4815ad7bd0445d55bc62c9f05674d
Mihai Rotaru <mihai_rotaru@lavabit.com>
2012-06-13 14:17:11 +0100

    problem: 'Use of undefined constant DB_SERVER"
    - added some functionality for the `MySQLDatabase` class
    - started getting warnings about somethings from PEAR being deprecated,
      so I deactivated deprecation warnings from php.ini, on line 516:
      `error_reporting = E_ALL & ~E_DEPRECATED ; on line 516`
    - another issue:
      --------------------------------------------------------------------------------
      Notice: Use of undefined constant DB_SERVER - assumed 'DB_SERVER' in
      E:\pdev\xampp\htdocs\imgshare\include\database.php on line 19
    
      Notice: Use of undefined constant DB_USER - assumed 'DB_USER' in
      E:\pdev\xampp\htdocs\imgshare\include\database.php on line 19
    
      Notice: Use of undefined constant DB_PASS - assumed 'DB_PASS' in
      E:\pdev\xampp\htdocs\imgshare\include\database.php on line 19
    
      Warning: mysql_connect() [function.mysql-connect]: php_network_getaddresses:
      getaddrinfo failed: No such host is known. in
      E:\pdev\xampp\htdocs\imgshare\include\database.php on line 19
    
      Warning: mysql_connect() [function.mysql-connect]: [2002]
      php_network_getaddresses: getaddrinfo failed: No such host is known. (trying to
      connect via tcp://DB_SERVER:3306) in
      E:\pdev\xampp\htdocs\imgshare\include\database.php on line 19
    
      Warning: mysql_connect() [function.mysql-connect]: php_network_getaddresses:
      getaddrinfo failed: No such host is known. in
      E:\pdev\xampp\htdocs\imgshare\include\database.php on line 19 Cannot connect to
      database: php_network_getaddresses: getaddrinfo failed: No such host is known.
      --------------------------------------------------------------------------------
    - tried for a while, but couldn't find the problem; will work around it,
      and post a question to StackOverflow if I won't come up with a solution.

--- include/database.php
+++ include/database.php
@@ -22,7 +22,7 @@ class MySQLDatabase {
         } else {
             $db_select = mysql_select_db( DB_NAME, $this->connection );
             if( !$db_select ) {
-                die) "Database selection failed: " . mysql_error() );
+                die( "Database selection failed: " . mysql_error() );
             }
         }
     }
@@ -34,8 +34,44 @@ class MySQLDatabase {
             unset( $this->connection );
         }
     }
+
+    //--------------------------------------------------------------------------
+    public function query( $sql ) {
+        $result = mysql_query( $sql, $this->connection );
+        $this->confirm_query( $result );
+        return $result;
+    }
+
+    //--------------------------------------------------------------------------
+    private function confirm_query( $result ) {
+        if( !$result ) {
+            die( "Database query failed: " . mysql_error() );
+        }
+    }
+
+    // sanitize $string, to make it SQL-safe
+    // ~from: Lynda.com - Essential PHP with MySQL, 13-06
+    //--------------------------------------------------------------------------
+    function mysql_prep( $string ) {
+        $magic_quotes_active = get_magic_quotes_gpc();
+        $new_enough_php = function_exists( "mysql_real_escape_string" ); // i.e. PHP >= v4.3.0
+
+        if( $new_enough_php ) { // PHP v4.3.0 or higher
+            // undo any magic quote effects so mysql_real_escape_string can do the work
+            if( $magic_quotes_active ) { 
+                $string = stripslashes( $string ); 
+            }
+            $string = mysql_real_escape_string( $string );
+        } else  { // before PHP v4.3.0
+            // if magic quotes aren't already on then add slashes manually
+            if( !$magic_quotes_active ) { 
+                $string = addslashes( $string ); 
+            }
+            // if magic quotes are active, then the slashes already exist
+        }
+        return $string;
+    }
 }
 
 $database = new MySQLDatabase();
-$
 ?>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
-phpinfo();
+require_once( "../include/database.php" );
+
+if( isset( $database ) ) { echo "TRUE"; } else { echo "FALSE"; }
+echo "<br/>";
+echo "it's working";
 ?>
