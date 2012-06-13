b3c188eb8ffe52266895d0f48879666612b9bd57
Mihai Rotaru <mihai_rotaru@lavabit.com>
2012-06-13 16:46:09 +0100

    new functions, refactoring, better error display

--- include/database.php
+++ include/database.php
@@ -2,7 +2,7 @@
 require_once("config.php");
 
 class MySQLDatabase {
-    
+
     // constants
     const DB_SERVER = 'localhost';
     const DB_USER = 'gallery';
@@ -10,13 +10,17 @@ class MySQLDatabase {
     const DB_NAME = 'imgshare';
 
     // attributes
-    //--------------------------------------------------------------------------
     private $connection;
+    private $magic_quotes_active;
+    private $real_escape_string_exists;
+    public $last_query;
 
     // constructors / destructors
     //--------------------------------------------------------------------------
     function __construct() {
         $this->open_connection();
+        $this->magic_quotes_active = get_magic_quotes_gpc();
+        $this->real_escape_string_exists = function_exists( "mysql_real_escape_string" ); // i.e. PHP >= v4.3.0
     }
 
     // methods
@@ -43,6 +47,7 @@ class MySQLDatabase {
 
     //--------------------------------------------------------------------------
     public function query( $sql ) {
+        $this->last_query = $sql;
         $result = mysql_query( $sql, $this->connection );
         $this->confirm_query( $result );
         return $result;
@@ -51,31 +56,48 @@ class MySQLDatabase {
     //--------------------------------------------------------------------------
     private function confirm_query( $result ) {
         if( !$result ) {
-            die( "Database query failed: " . mysql_error() );
+            $output = "<div class=\"error\">Database query failed.<br/>Message: <span class=\"mysql_error\">" . mysql_error() . "</span><br/>";
+            $output .="Query: <code>" . $this->last_query . "</code></div>";
+            die( $output );
         }
     }
 
-    // sanitize $string, to make it SQL-safe
-    // ~from: Lynda.com - Essential PHP with MySQL, 13-06
+    // sanitize $string, to make it safe for database engines
     //--------------------------------------------------------------------------
-    function mysql_prep( $string ) {
-        $magic_quotes_active = get_magic_quotes_gpc();
-        $new_enough_php = function_exists( "mysql_real_escape_string" ); // i.e. PHP >= v4.3.0
-
-        if( $new_enough_php ) { // PHP v4.3.0 or higher
+    function escape_value( $string ) {
+        if( $this->real_escape_string_exists ) { // PHP v4.3.0 or higher
             // undo any magic quote effects so mysql_real_escape_string can do the work
-            if( $magic_quotes_active ) { 
-                $string = stripslashes( $string ); 
-            }
+            if( $this->magic_quotes_active ) { $string = stripslashes( $string ); }
             $string = mysql_real_escape_string( $string );
         } else  { // before PHP v4.3.0
             // if magic quotes aren't already on then add slashes manually
-            if( !$magic_quotes_active ) { 
+            if( !$this->magic_quotes_active ) { 
                 $string = addslashes( $string ); 
             }
             // if magic quotes are active, then the slashes already exist
         }
         return $string;
+    }
+
+    //--------------------------------------------------------------------------
+    function fetch_array( $result_set ) {
+        return mysql_fetch_array( $result_set);
+    }
+
+    //--------------------------------------------------------------------------
+    public function num_rows($result_set) {
+        return mysql_num_rows($result_set);
+    }
+
+    //--------------------------------------------------------------------------
+    public function insert_id() {
+        // get the last id inserted over the current db connection
+        return mysql_insert_id($this->connection);
+    }
+
+    //--------------------------------------------------------------------------
+    public function affected_rows() {
+        return mysql_affected_rows($this->connection);
     }
 }
 
--- public/index.php
+++ public/index.php
@@ -1,7 +1,30 @@
 <?php
 require_once( "../include/database.php" );
-
-if( isset( $database ) ) { echo "TRUE"; } else { echo "FALSE"; }
-echo "<br/>";
-echo "it's working";
 ?>
+
+<html>
+	<head>
+		<title>OO PHP</title>
+        <link rel="stylesheet" href="css/style.css"/>
+	</head>
+    <body>
+    <?php
+
+    if( isset( $database ) ) { echo "TRUE"; } else { echo "FALSE"; }
+    echo( "<br/>" );
+    echo $database->escape_value( "It's working?<br/>" );
+
+    //$sql  = "INSERT INTO users (id, username, hashed_password, first_name, last_name, email) ";
+    //$sql .= "VALUES (1, 'derp', 'secretpwd', 'Derp', 'Derpowsky', 'derp@mail.com')";
+    //$result = $database->query($sql);
+
+    $sql = "SELECT * FROM users WHERE id = 1";
+    $result_set = $database->query($sql);
+    $found_user =  mysql_fetch_array($result_set);
+    echo $found_user['username'];
+
+    ?>
+    </body>
+</html>
+
+
